1b100e08b1c489e148d89a70f59238dd77d065fd
Daliys <Daliys133@gmail.com>
2021-06-06 01:21:36 +0300

    Added constant camera view and aiming

--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -7,13 +7,14 @@ public class MainCharacterController : MonoBehaviour
     public float speed;
     public float turnSmothTime = 0.1f;
     public float turnSmothVelocity;
-    private Transform camera;
+    private Camera camera;
     private Animator animator;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
     public float speedMovementAnimstion;
     public CharacterController characterController;
 
+    public Vector3 mousePosition;
 
     private Vector3 velocity;
     public Transform groundCheck;
@@ -26,13 +27,33 @@ public class MainCharacterController : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        camera = Camera.main.transform;
+        camera = Camera.main;
    
     }
 
 
     void Update()
     {
+        //mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        //print(Input.mousePosition);
+
+         Vector3 worldPosition;
+        Plane plane = new Plane(Vector3.up, 0);
+
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            worldPosition = ray.GetPoint(distance);
+            print(worldPosition);
+            //worldPosition.y = transform.position.y + transform.localScale.y;
+            Vector2 lookDir = new Vector2(worldPosition.x, worldPosition.z) - new Vector2(transform.position.x, transform.position.z);
+
+            float angle = Mathf.Atan2(lookDir.x, lookDir.y) * Mathf.Rad2Deg + 180f;
+            transform.eulerAngles = new Vector3(0, angle, 0);
+           // transform.LookAt(worldPosition);
+        }
+
 
         isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -67,9 +88,9 @@ public class MainCharacterController : MonoBehaviour
         Vector3 moveDirection = Vector3.zero;
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.transform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmothVelocity, turnSmothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             //print(movementHorizontal + "   " + moveDirection.x);
@@ -81,5 +102,14 @@ public class MainCharacterController : MonoBehaviour
 
         //transform.position += move*Time.deltaTime;
         characterController.Move(move * Time.deltaTime);
+    }
+
+
+
+    private void FixedUpdate()
+    {
+      //  Vector2 lookDir = new Vector2(mousePosition.x, mousePosition.z) - new Vector2(transform.position.x, transform.position.z);
+       // float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        //transform.eulerAngles = new Vector3(0,angle,0);
     }
 }
